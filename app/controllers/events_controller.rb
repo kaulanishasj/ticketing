361ff35eefc566621bdf3cce957ac315aae6dfa1
Anisha Kaul <anisha.kaul@sourcebits.com>
2013-09-09 18:42:40 +0530

    removed the nonsense comments

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,31 +6,24 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @event }
     end
   end
 
-  # GET /events/new
-  # GET /events/new.json
   def new
     @event = Event.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @event }
     end
   end
 
-  # GET /events/1/edit
   def edit
     @event = Event.find(params[:id])
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(params[:event])
 
@@ -45,8 +38,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PUT /events/1
-  # PUT /events/1.json
   def update
     @event = Event.find(params[:id])
 
@@ -61,8 +52,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
