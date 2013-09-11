4276c6f9817cc96dad3b329a779ee89c943be0f8
Anisha Kaul <anisha.kaul@sourcebits.com>
2013-09-12 02:42:34 +0530

    fixed the gender thing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
 
-  def gender_html_options
+  def gender_html_options(gender_val)
     options = "<option value> Would not like to mention </option>"
-		['Male', 'Female'].each do |gender|
-			options << "<option value=\"#{gender}\">#{gender}</option>" 
+		["Male", "Female"].each do |gender|
+			if gender_val == gender
+				options << "<option selected=\"selected\" value=\"#{gender}\">#{gender}</option>" 
+			else
+				options << "<option value=\"#{gender}\">#{gender}</option>" 
+			end
 		end
     options.html_safe
   end
